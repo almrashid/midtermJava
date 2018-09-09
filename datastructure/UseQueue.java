0fde5725690c5908ba18e62e0f193ca62a0a5bb2
almrashid <alrashidm@outlook.com>
2018-09-09 16:42:29 -0400

    queue

--- datastructure/UseQueue.java
+++ datastructure/UseQueue.java
@@ -1,14 +1,28 @@
 package datastructure;
 
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class UseQueue {
 
 	public static void main(String[] args) {
 		/*
 		 * Demonstrate how to use Queue that includes add,peek,remove,pool elements.
 		 * Use For Each loop and while loop with Iterator to retrieve data.
-		 * 
+		 *
 		 */
 
-	}
+		Queue<Integer> queue = new LinkedList<>();
 
+		queue.add(10);
+		queue.add(20);
+		queue.add(30);
+		Iterator it = queue.iterator();
+
+
+		while (it.hasNext()){
+			System.out.println(it.next());
+		}
+	}
 }
